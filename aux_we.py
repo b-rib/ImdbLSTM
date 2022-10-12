5cb3ee4e0f37dac7cd8ed3f27d67ece980862561
b-rib <bferrib@gmail.com>
2022-10-12 15:44:22 -0300

    files loaded

---
+++ aux_we.py
@@ -0,0 +1,58 @@
+import io
+import os
+import re
+import shutil
+import string
+import tqdm
+# import kormos
+import tensorflow as tf
+import numpy as np
+from tensorflow.python.keras import Sequential, layers
+from tensorflow.python.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+from keras.layers import TextVectorization
+
+def generate_training_data(sequences, window_size, neg_samples, vocab_size, seed):
+  # Elements of each training example are appended to these lists.
+  targets, contexts, labels = [], [], []
+
+  # Build the sampling table for `vocab_size` tokens.
+  sampling_table = tf.keras.preprocessing.sequence.make_sampling_table(vocab_size)
+
+  # Iterate over all sequences (sentences) in the dataset.
+  for sequence in tqdm.tqdm(sequences):
+
+    # Generate positive skip-gram pairs for a sequence (sentence).
+    positive_skip_grams, _ = tf.keras.preprocessing.sequence.skipgrams(
+          sequence,
+          vocabulary_size=vocab_size,
+          sampling_table=sampling_table,
+          window_size=window_size,
+          negative_samples=0)
+
+    # Iterate over each positive skip-gram pair to produce training examples
+    # with a positive context word and negative samples.
+    for target_word, context_word in positive_skip_grams:
+      context_class = tf.expand_dims(
+          tf.constant([context_word], dtype="int64"), 1)
+      negative_sampling_candidates, _, _ = tf.random.log_uniform_candidate_sampler(
+          true_classes=context_class,
+          num_true=1,
+          num_sampled=neg_samples,
+          unique=True,
+          range_max=vocab_size,
+          seed=seed,
+          name="negative_sampling")
+
+      # Build context and label vectors (for one target word)
+      negative_sampling_candidates = tf.expand_dims(
+          negative_sampling_candidates, 1)
+
+      context = tf.concat([context_class, negative_sampling_candidates], 0)
+      label = tf.constant([1] + [0]*neg_samples, dtype="int64")
+
+      # Append each element from the training example to global lists.
+      targets.append(target_word)
+      contexts.append(context)
+      labels.append(label)
+
+  return targets, contexts, labels
